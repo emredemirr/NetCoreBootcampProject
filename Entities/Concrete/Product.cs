adfa20646429ef7bede388b547d6b5dd530097dc
Emre Demir <emreedemir@yahoo.com>
2023-05-30 15:59:58 +0300

    Product and  Category entities added

--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -8,6 +8,11 @@ namespace Entities.Concrete
 {
     public class Product
     {
+        public int ProductId { get; set; }
+        public int CategoryId{ get; set; }
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
 
     }
 }
